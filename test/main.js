b3b29b04b05587b1b845be7204d81015a305d08f
kikuchy <kikuchy.h@gmail.com>
2015-02-24 10:32:24 +0900

    add a test case using github repositories

--- test/main.js
+++ test/main.js
@@ -4,6 +4,17 @@ var gutil = require('gulp-util');
 var fs = require('fs');
 var pj = require('path').join;
 
+var createVinyl = function (indexFileName) {
+    var base = pj(__dirname, 'fixtures');
+    var filePath = pj(base, indexFileName);
+    return new gutil.File({
+        cwd: __dirname,
+        base: base,
+        path: filePath,
+        contents: fs.readFileSync(filePath)
+    });
+};
+
 describe('gulp-duo', function () {
     describe('duo()', function () {
 
@@ -37,14 +48,7 @@ describe('gulp-duo', function () {
     });
 
     it('should build single javascript file requires one local file', function (done) {
-        var base = pj(__dirname, 'fixtures');
-        var filePath = pj(base, 'index.js');
-        var targetFile = new gutil.File({
-            cwd: __dirname,
-            base: base,
-            path: filePath,
-            contents: fs.readFileSync(filePath)
-        });
+        var targetFile = createVinyl('index.js');
         var stream = duo();
         stream.once('data', function (file) {
             should.exist(file);
@@ -53,7 +57,22 @@ describe('gulp-duo', function () {
             should.exist(file.contents);
             file.path.should.equal(pj(__dirname, 'fixtures', 'index.js'));
             String(file.contents).should.equal(fs.readFileSync(pj(__dirname, 'expect', 'index.js'), 'utf8'));
-        }).on('end', function () {
+            done();
+        });
+        stream.write(targetFile);
+        stream.end();
+    });
+
+    it('should build single javascript file requires two github repositories', function (done) {
+        var targetFile = createVinyl('github_index.js');
+        var stream = duo();
+        stream.once('data', function (file) {
+            should.exist(file);
+            should.exist(file.path);
+            should.exist(file.relative);
+            should.exist(file.contents);
+            file.path.should.equal(pj(__dirname, 'fixtures', 'github_index.js'));
+            String(file.contents).should.equal(fs.readFileSync(pj(__dirname, 'expect', 'github_index.js'), 'utf8'));
             done();
         });
         stream.write(targetFile);
